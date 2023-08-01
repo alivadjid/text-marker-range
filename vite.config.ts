418ff504f2f9b61f4feba52ad2813aecca765e4b
alivadjid <snub01@mail.ru>
2023-08-01 23:15:59 +0300

    build with vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue()],
+
   server: {
     port: 5000,
   },
@@ -15,5 +17,26 @@ export default defineConfig({
     },
   },
 
-  plugins: [vue()],
+  build: {
+    target: "es2022",
+    cssCodeSplit: true,
+    copyPublicDir: false,
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "TextKey",
+      fileName: "text-key-lib",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
 });
