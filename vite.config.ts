1c9c7e10c614c64b1dbb8e9b102318841022f52d
alivadjid <snub01@mail.ru>
2023-08-04 21:36:52 +0300

    update config add ts lib

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import * as path from "path";
 
 import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
 
   server: {
     port: 5000,
@@ -28,12 +29,8 @@ export default defineConfig({
       fileName: "text-key-lib",
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ["vue"],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           vue: "Vue",
         },
