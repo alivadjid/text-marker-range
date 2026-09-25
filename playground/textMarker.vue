840ce006d6fff65fd7512ccde1aaddb0d346453b
alivadjid <snub01@mail.ru>
2026-09-25 11:11:28 +0300

    #4: optimization

---
+++ playground/textMarker.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>textMarker.vue</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+</style>
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+import { default as TextDataKeyWithExpansion } from './TextDataKeyWithExpansion.vue'
